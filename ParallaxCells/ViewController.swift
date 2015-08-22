d4162df6ada4ff9a227a6613a451d8e97e3ddbb1
Krishan Patel <krishan@rockolabs.com>
2015-08-22 03:32:34 +0100

    Initial Commit

---
+++ ParallaxCells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParallaxCells
+//
+//  Created by Krishan Patel on 22/08/2015.
+//  Copyright (c) 2015 Rocko Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
